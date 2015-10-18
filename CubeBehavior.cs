7386a23de114b8443c98e4917b6a3b6682f6d375
farseeraslan <tcb14@hampshire.edu>
2015-10-18 16:24:13 -0400

    Part 1
    Scripts and HTML for the first part of the Transportation project

---
+++ CubeBehavior.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CubeBehavior : MonoBehaviour {
+	GameController theGameController;
+
+	void Start () {
+		theGameController = GameObject.Find ("GameControllerObject").GetComponent<GameController> ();
+	}
+
+	void Update () {
+	
+	}
+
+	void OnMouseDown(){
+		theGameController.ProcessClickedCube (this.gameObject);
+	}
+
+}
---
+++ GameController.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameController : MonoBehaviour {
+	private GameObject[] allCubes;
+	public GameObject cubePrefab;
+	public Material clickMaterial;
+	public Material mainMaterial;
+	int numCubes = 16;
+
+	void Start () {
+		allCubes = new GameObject[numCubes];
+		for (int i = 0; i < numCubes; i++) {
+			allCubes [i] = (GameObject) Instantiate(cubePrefab, new Vector3(i*1.5f - 11,0,2.5f), Quaternion.identity);
+		}
+	}
+
+	void Update () {
+	
+	}
+
+	public void ProcessClickedCube(GameObject clickedCube){
+		foreach (GameObject oneCube in allCubes) {
+			 oneCube.GetComponent<Renderer> ().material = mainMaterial;
+		}
+		clickedCube.GetComponent<Renderer> ().material = clickMaterial;
+	}
+
+}
